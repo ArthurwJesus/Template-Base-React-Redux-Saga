095c6adf4ab3fa055888517ad5310e66c87ad0d3
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2022-10-18 10:35:30 -0300

    Update Dashboard.js

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Lista from "../../components/lista";
+import ListById from "../../components/listbyId";
 
 
 const Dashboard = () =>{
     return(
         <React.Fragment>
-            <p>Personagens:</p>
+            <p>GET ALL:</p>
             <Lista/>
+            <p>GET BY ID:</p>
+            <ListById/>
         </React.Fragment>
     )
 }
