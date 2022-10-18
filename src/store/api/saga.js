c89614ffdd2533133c1a2bdf0f731daeb787a080
Arthur Jesus <72893926+ArthurwJesus@users.noreply.github.com>
2022-10-18 10:34:56 -0300

    Update saga.js

--- src/store/api/saga.js
+++ src/store/api/saga.js
@@ -2,19 +2,22 @@ import {call,put,takeEvery} from "redux-saga/effects"
 
 import {
     getPoke,
+    getPokeId,
     postPoke,
     delPoke,
     putPoke
 } from "../../helpers/backend"
 
 import{
-    listRicMorty,
     listRicMortySucces,
-    listRicMortyError
+    listRicMortyError,
+    listRicMortyIdSuccess,
+    listRicMortyIdError
 } from "../api/action"
 
 import{
     LIST_RICKMORTY,
+    LIST_RICKMORTY_ID
 } from "./actionsType"
 
 
@@ -24,6 +27,15 @@ function* listandoRickMorty({payload:{}}){
         yield put(listRicMortySucces(response.results))
     }catch(error){
         yield put(listRicMortyError(error))
+    }
+}
+
+function* listandoRickMortyById({payload:{id}}){
+    try{
+        const response = yield call(getPokeId,id)
+        yield put(listRicMortyIdSuccess(response.results))
+    }catch(error){
+        yield put(listRicMortyIdError(error))
     }
 }
 
@@ -43,21 +55,22 @@ function* listandoRickMorty({payload:{}}){
 
 
 // Update example
-function* updateCadastroRickMorty({payload:{id,data}}){
-    try{
-        const response = yield call(putPoke,id,{
-            name: data.name
-        })
-        yield put(listRicMortySucces(response.data))
-    }catch(error){
-        yield put(listRicMortyError(error))
-    }
-}
+// function* updateCadastroRickMorty({payload:{id,data}}){
+//     try{
+//         const response = yield call(putPoke,id,{
+//             name: data.name
+//         })
+//         yield put(listRicMortySucces(response.data))
+//     }catch(error){
+//         yield put(listRicMortyError(error))
+//     }
+// }
 
 
 
 function* apiSaga(){
     yield takeEvery(LIST_RICKMORTY,listandoRickMorty)
+    yield takeEvery(LIST_RICKMORTY_ID,listandoRickMortyById)
 }
 
 export default apiSaga
